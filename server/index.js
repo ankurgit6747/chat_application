f69c83a579df56a8af79cba94bc49bb67a946ecc
ankurgit6747 <ankur02srma@gmail.com>
2023-02-11 21:40:54 +0530

    update

--- server/index.js
+++ server/index.js
@@ -16,28 +16,28 @@ app.use(cors())
 const users = [{}]
 
 app.get("/", (req, res) => {
-  res.send("HELL ITS WORKING");
+  res.send("ITS WORKING");
 })
 
 io.on("connection", (socket) => {
   console.log("New connection");
 
-  socket.on('joined', ({user}) => {
+  socket.on('joined', ({ user }) => {
     users[socket.id] = user;
     console.log(`${user} has joined`)
     socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
     socket.emit('welcome', { user: "Admin", message: `Welcome to the chat ${users[socket.id]}` });
   })
 
-  socket.on('message', ({message, id}) => {
-    io.emit('sendMessage', {user:users[id], message, id})
+  socket.on('message', ({ message, id }) => {
+    io.emit('sendMessage', { user: users[id], message, id })
   })
 
   socket.on('disconnectUser', () => {
-    socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left`})
+    socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left` })
     console.log("User left")
   })
-  
+
 })
 
 server.listen(port, () => {
